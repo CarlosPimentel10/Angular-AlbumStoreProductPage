29d6c96fbb396b99696a69413f4ed8674c4cf737
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-10-18 12:42:45 +0100

    created product, description and updated app-html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductPageComponent } from './product-page-component/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
