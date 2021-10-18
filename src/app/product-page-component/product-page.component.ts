2187d329d1620620b088f4d6d0896211d06c20b0
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-10-18 18:50:52 +0100

    changed app description

--- src/app/product-page-component/product-page.component.ts
+++ src/app/product-page-component/product-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-page',
+  selector: 'app-product-page','@app-product-description',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
