70051bcdaeb5d7f95778f28936d9fb1da7d7f867
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-10-22 21:10:41 +0100

    album and track interface added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { AlbumStorePage } from './../../e2e/app.po';
+import { Observable } from 'rxjs';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +12,7 @@ export class ProductService {
 
   private _albumUrl: '../AlbumStorePage';
 
-  getAlbum(id:number){
-    return this._http.get(this._albumUrl).map((response)=>response.json());
+  getAlbum(id:number): Observable<Album>{
+    return this._http.get(this._albumUrl).map((response)=><Album>response.json());
   }
 }
