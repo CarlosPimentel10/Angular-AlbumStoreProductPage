8f2a943a3fa6d6d3edeb87327bd9d2cebc34d80b
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-10-22 21:46:41 +0100

    passing 11 out of 8 tests

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -3,7 +3,6 @@ import { ProductService } from '../product.service';
 import { Album } from '../album';
 
 
-
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
