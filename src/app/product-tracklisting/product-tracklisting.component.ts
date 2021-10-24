414d11e988eea0624581c074b6e6d77a5ef0a1b9
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-10-24 12:35:11 +0100

    update

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albumInfo: Albumm;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
