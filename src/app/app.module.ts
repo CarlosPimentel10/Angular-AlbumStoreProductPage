373420119bf971c4a791da4bd5149ea4a70bcd1c
CarlosPimentel10 <carlos.costa.pimentel@gmail.com>
2021-11-24 10:48:10 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { ProductPageComponent } from './product-page-component/product-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
